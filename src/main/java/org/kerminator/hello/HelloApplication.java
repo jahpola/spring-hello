a0017426944e19caedf87df8054c2bdf5a6ed4e2
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-01-10 19:26:27 +0200

    Test upgrades

--- src/main/java/org/kerminator/hello/HelloApplication.java
+++ src/main/java/org/kerminator/hello/HelloApplication.java
@@ -2,12 +2,8 @@ package org.kerminator.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class HelloApplication {
 
 	public static void main(String[] args) {
