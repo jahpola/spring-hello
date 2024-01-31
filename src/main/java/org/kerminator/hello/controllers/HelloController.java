4d3e4f4174c9e89a95bbc5b4b804aa45c5eaa76f
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2024-01-31 15:23:51 +0200

    Initial Otel

--- src/main/java/org/kerminator/hello/controllers/HelloController.java
+++ src/main/java/org/kerminator/hello/controllers/HelloController.java
@@ -1,10 +1,14 @@
 package org.kerminator.hello.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("/")
     public String hello() {
