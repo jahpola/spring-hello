64bfde83fe84516d24934429fef2c4586e293d1a
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-04-11 19:58:21 +0300

    Sonar test 3

--- src/main/java/org/kerminator/hello/controllers/HelloController.java
+++ src/main/java/org/kerminator/hello/controllers/HelloController.java
@@ -2,8 +2,7 @@ package org.kerminator.hello.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,12 +10,12 @@ public class HelloController {
 
     Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping(path="/", method={RequestMethod.GET})
+    @GetMapping(path = "/")
     public String hello() {
         return "Hello World";
     }
     
-    @RequestMapping(path="/test", method={RequestMethod.GET})
+    @GetMapping(path="/test")
     public String test() {
         return "Test Test";
     }
