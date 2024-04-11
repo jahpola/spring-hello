99e7386d8a0a8d50480d102d8bc78a7541a172ae
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-04-11 21:34:07 +0300

    Tests!

--- src/test/java/org/kerminator/hello/HelloApplicationTests.java
+++ src/test/java/org/kerminator/hello/HelloApplicationTests.java
@@ -1,13 +1,31 @@
 package org.kerminator.hello;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class HelloApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testWithMockMvc(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hello World"));
+	}
+
+	void testtestWithMockMvc(@Autowired MockMvc mvc) throws Exception {
+		mvc.perform(get("/test")).andExpect(status().isOk()).andExpect(content().string("Test Test"));
+	}
 }
