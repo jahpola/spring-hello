413ec3a3a150d61d62271c7d52005975db97a075
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2024-04-11 12:55:29 +0300

    Sonarqube test 2

--- src/main/java/org/kerminator/hello/controllers/HelloController.java
+++ src/main/java/org/kerminator/hello/controllers/HelloController.java
@@ -3,6 +3,7 @@ package org.kerminator.hello.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,12 +11,12 @@ public class HelloController {
 
     Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    @RequestMapping("/")
+    @RequestMapping(path="/", method={RequestMethod.GET})
     public String hello() {
         return "Hello World";
     }
     
-    @RequestMapping("/test")
+    @RequestMapping(path="/test", method={RequestMethod.GET})
     public String test() {
         return "Test Test";
     }
