248165402d6454219abf4ff7d9a5d1fea18b85a9
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2024-05-21 11:33:48 +0300

    Lombok

--- src/main/java/org/kerminator/hello/model/Product.java
+++ src/main/java/org/kerminator/hello/model/Product.java
@@ -1,8 +1,14 @@
 package org.kerminator.hello.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "product")
 public class Product {
     
@@ -10,44 +16,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-	@Column(name = "firstname")
     private String firstname;
-
-	@Column(name = "lastname")
 	private String lastname;
-
-    public Product() {}
 
 	public Product(String firstname, String lastname) {
 		this.firstname = firstname;
 		this.lastname = lastname;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getFirstname() {
-		return firstname;
-	}
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-	public String getLastname() {
-		return lastname;
-	}
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-    
-	@Override
-	public String toString() {
-		return String.format(
-				"Product[id=%d, firstname='%s', lastname='%s']",
-				id, firstname, lastname);
 	}
 
 }
