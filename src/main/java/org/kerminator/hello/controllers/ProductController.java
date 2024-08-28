8c6c8f68e762c05a6fc3635ce8224994acf44558
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-08-28 21:52:48 +0300

    Clean up+tests

--- src/main/java/org/kerminator/hello/controllers/ProductController.java
+++ src/main/java/org/kerminator/hello/controllers/ProductController.java
@@ -1,31 +1,30 @@
 package org.kerminator.hello.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.kerminator.hello.model.Product;
 import org.kerminator.hello.repository.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class ProductController {
-
-    @Autowired
-    ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     @GetMapping("/products")
     public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required = false) String lastname) {
@@ -42,7 +41,7 @@ public class ProductController {
 
             return new ResponseEntity<>(products, HttpStatus.OK);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     
@@ -53,7 +52,7 @@ public class ProductController {
                 .save(new Product(product.getFirstname(),product.getLastname()));
             return new ResponseEntity<>(_product, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
--- src/test/java/org/kerminator/hello/HelloApplicationTests.java
+++ src/test/java/org/kerminator/hello/HelloApplicationTests.java
@@ -1,10 +1,7 @@
 package org.kerminator.hello;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -14,9 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 @SpringBootTest
@@ -30,19 +26,6 @@ class HelloApplicationTests {
 	private ObjectMapper objectMapper;
 
 	@Test
-	void contextLoads() {}
-
-	@Test
-	void root() throws Exception {
-		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Hello World"));
-	}
-
-	@Test
-	void testtest() throws Exception {
-		mockMvc.perform(get("/test")).andExpect(status().isOk()).andExpect(content().string("Test Test"));
-	}
-
-	@Test
 	void getemptyproduct() throws Exception {
 		mockMvc.perform(get("/products")).andExpect(status().is2xxSuccessful());
 	}
@@ -52,11 +35,21 @@ class HelloApplicationTests {
 		Product product = new Product("Testi","Ukko");
 
 		mockMvc.perform( MockMvcRequestBuilders
-				.post("/products")
-				.content(objectMapper.writeValueAsString(product))
-				.contentType(MediaType.APPLICATION_JSON));
-
-
-		//);
+				  .post("/products")
+				  .content(objectMapper.writeValueAsString(product))
+				  .contentType(MediaType.APPLICATION_JSON))
+				  .andExpect(status().is2xxSuccessful())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
 	}
+
+ 	@Test
+	void deleteproduct() throws Exception {
+		//Product product = new Product("Test", "Kuu");
+
+		mockMvc.perform ( MockMvcRequestBuilders
+				.delete("/products/{id}", "1")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNoContent());
+	}
+
 }
