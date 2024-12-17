385088bf5b90adaf974331354cf5fbab78a28a7c
Jyrki Ahpola <16726281+jahpola@users.noreply.github.com>
2024-12-17 22:13:14 +0200

    Try tests

--- src/test/java/org/kerminator/hello/HelloControllerTests.java
+++ src/test/java/org/kerminator/hello/HelloControllerTests.java
@@ -4,53 +4,79 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.kerminator.hello.controllers.ProductController;
 import org.kerminator.hello.service.ProductService;
+import org.kerminator.hello.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(ProductController.class)
 class HelloControllerTests {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Autowired
-	private ObjectMapper objectMapper;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-	@MockitoBean
+    @MockitoBean
     private ProductService productService;
 
+    private Product product;
 
-//    @BeforeAll
-//    public void init() {
-//        Product product = Product.builder()
-//                .id(1L)
-//                .name("nakki")
-//                .description("nakki teline")
-//                .price(BigDecimal.valueOf(10.15))
-//                .build();
-//        //when(productRepository.findById(1L).thenReturn(Optional.of(product));
-//    }
-    //@Test
-//    void shouldCreateProduct() throws Exception {
-//
-//
-//        mvc.perform(post("/products")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(product)))
-//                .andExpect(status().isCreated());
-//                //.andExpect(jsonPath("$.name").value("Nakki Teline"))
-//                //.andExpect(jsonPath("$.price").value(10.15));
-//    }
+    @BeforeEach
+    public void setUp() {
+        product = Product.builder()
+                .id(1L)
+                .name("nakki")
+                .description("nakki teline")
+                .price(BigDecimal.valueOf(10.15))
+                .build();
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/api/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(product)))
+                .andExpect(status().isCreated());
+                //.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("nakki"));
+                //.andExpect(jsonPath("$.price").value(10.15));
+    }
+
+    // @Test
+    // void find_existingProduct() throws Exception {
+    //     mvc.perform(get("/api/products/1")).andExpect(status().isOk());
+    // }
+
+    // @Test
+    // void findAllProducts() throws Exception {
+    //     mvc.perform(get("/products")).andExpect(status().isOk());
+    // }
 
     @Test
     void find_nonExistingProduct() throws Exception {
         mvc.perform(get("/products/100")).andExpect(status().isNotFound());
     }
 
+    @Test
+    void delete_nonExistingProduct() throws Exception {
+        mvc.perform(delete("/products/100")).andExpect(status().isNotFound());
+    }
 
-} 
+}
--- src/test/java/org/kerminator/hello/HelloRepositoryTests.java
+++ src/test/java/org/kerminator/hello/HelloRepositoryTests.java
@@ -1,6 +1,5 @@
 package org.kerminator.hello;
 
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
