bfca41f21979672eec1beaa855576add9bcd6c40
Jyrki Ahpola <jahpola@users.noreply.github.com>
2025-09-09 18:06:44 +0300

    Remove extra libs

--- src/test/java/org/kerminator/hello/HelloControllerTests.java
+++ src/test/java/org/kerminator/hello/HelloControllerTests.java
@@ -22,10 +22,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(ProductController.class)
 class HelloControllerTests {
